e4bd1521d631ab9cc2d4a16ddf23a9ec6b43a3f9
Sayan Samanta <sayans0323@gmail.com>
2025-01-12 00:44:44 +0530

    change_new

--- Dandadan/src/services/submit.js
+++ Dandadan/src/services/submit.js
@@ -5,19 +5,19 @@ import { auth } from "../config/regester";
 //submit button
 const submit = document.getElementById('submit');
 submit.addEventListener("click", function (event) {
+    event.preventDefault();
     // get input
     const user = document.getElementById('user').value;
     const email = document.getElementById('email').value;
-    // email = email.toLowerCase();
     const password = document.getElementById('password').value;
 
     // Check if fields are empty
     if (user && email && password) {
-        validEmail(event);
+        validEmail(email, password);
     }
 });
 
-function validEmail(event){
+function validEmail(email, password) {
     const gmailRegex = /^[a-zA-Z0-9._%+-]+@iiserb\.ac\.in$/;
         const gmailRegex24 = /^[a-zA-Z._%+-]+24@iiserb\.ac\.in$/;
         if (!gmailRegex.test(email))
@@ -28,13 +28,13 @@ function validEmail(event){
                 alert("Only for 24 Batch");
 
             else {
-                Submit(event);
+                Submit(email, password);
             }
         }
 }
 
-function Submit(event) {
-    event.preventDefault();
+function Submit(email, password) {
+    
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed up 
@@ -43,7 +43,7 @@ function Submit(event) {
             // ...
         })
         .catch((error) => {
-            const errorCode = error.code;
+            // const errorCode = error.code;
             const errorMessage = error.message;
             alert(errorMessage);
             // ..
