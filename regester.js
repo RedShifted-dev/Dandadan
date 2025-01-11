6d57d87a12f56f69274a4b479a67568c52bcd8e7
Sayan Samanta <sayans0323@gmail.com>
2025-01-11 18:58:03 +0530

    change

--- regester.js
+++ regester.js
@@ -21,13 +21,26 @@ const submit = document.getElementById('submit');
 submit.addEventListener("click", function(event){
     // get input
     const user = document.getElementById('user').value;
-    const email = document.getElementById('mail').value;
+    const email = document.getElementById('email').value;
+    // email = email.toLowerCase();
     const password = document.getElementById('password').value;
 
     // Check if fields are empty
     if (user && email && password)
     {
-        event.preventDefault();
-        alert(5);
+        const gmailRegex = /^[a-zA-Z0-9._%+-]+@iiserb\.ac\.in$/;
+        const gmailRegex24 = /^[a-zA-Z._%+-]+24@iiserb\.ac\.in$/;
+        if (!gmailRegex.test(email))
+            alert("Enter IISER-B Email ID");
+        else
+        {
+            if(!gmailRegex24.test(email))
+                alert("Only for 24 Batch")
+            else
+            {
+                event.preventDefault();
+                alert(5);
+            }
+        }
     }
 })
