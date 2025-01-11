6c2d8206a2bcdae044a7cd0819ee20263b3e8882
Sayan Samanta <sayans0323@gmail.com>
2025-01-11 17:17:15 +0530

    First Commit

---
+++ auth.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector('.wrapper');
+const loginLnik = document.querySelector('.login-link');
+const registerLnik = document.querySelector('.register-link');
+// const btnpopup = document.querySelector('.btnLogin-popup');
+// const iconClose = document.querySelector('.icon-close');
+
+registerLnik.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLnik.addEventListener('click', ()=> {
+    console.log('login');
+    wrapper.classList.remove('active');
+});
+
+// btnpopup.addEventListener('click', ()=> {
+//     wrapper.classList.add('active-popup');
+// });
+
+// iconClose.addEventListener('click', ()=> {
+//     wrapper.classList.remove('active-popup');
+// });
