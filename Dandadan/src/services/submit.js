b69a8ebccfadc8938968d59091ea819ffaf49ef1
Sayan Samanta <sayans0323@gmail.com>
2025-01-17 01:08:00 +0530

    add Firestore

--- Dandadan/src/services/submit.js
+++ Dandadan/src/services/submit.js
@@ -1,32 +1,44 @@
 import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js";
 import { auth } from "../config/regester.js";
-import { validEmail } from "../logic/validEmail.js";
+import { app } from "../config/regester.js";
+// import { validEmail } from "../logic/validEmail.js";
+
+
+// Initialize Firestore
+const db = getFirestore(app);
 
 
 //submit button
 const submit = document.getElementById('submit');
+
 submit.addEventListener("click", function (event) {
     event.preventDefault();
     // get input
-    const user = document.getElementById('user').value;
+    const userName = document.getElementById('user').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     // Check if fields are empty
-    if (user && email && password) {
-        validEmail(email);
+    if (userName && email && password) {
+        // validEmail(email);
+        Submit(userName, email, password);
     }
 });
 
-export function Submit(email, password) {
-    
+// export
+    function Submit(userName, email, password) {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
-            // Signed up 
             const user = userCredential.user;
-            alert("done");
+            setDoc(doc(db, "users", user.uid), {
+                uid: user.uid,
+                user: userName,
+                email: email,
+            }).then(() => {
+                alert("done");
+            });
             
-            // ...
         })
         .catch((error) => {
             // const errorCode = error.code;
